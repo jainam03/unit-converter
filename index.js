26c9b8fe78d10179bab8e74df116657446ad8341
jainam03 <jbchheda03@gmail.com>
2022-12-12 12:05:33 +0530

    added conversion logic

---
+++ index.js
@@ -0,0 +1,24 @@
+let inputEl = document.getElementById("input")
+let convertBtn = document.getElementById("convert-btn")
+let lengthValues = document.getElementById("length-values")
+let volumeValues = document.getElementById("volume-values")
+let massValues = document.getElementById("mass-values")
+
+convertBtn.addEventListener("click", function () {
+    if (inputEl.value === "") {
+        alert("Please enter a value")
+    }
+    let feetValue = inputEl.value * 3.28
+
+    lengthValues.innerHTML = `${inputEl.value} meters = ${feetValue} feet | ${feetValue} feet = ${inputEl.value} meters`
+
+
+    let gallonValue = inputEl.value * 0.26
+
+    volumeValues.innerHTML = `${inputEl.value} litres = ${gallonValue} gallons | ${gallonValue} gallons = ${inputEl.value} litres`
+
+    let poundValues = inputEl.value * 2.21
+
+    massValues.innerHTML = `${inputEl.value} Kilos = ${poundValues} pounds | ${poundValues} pounds = ${inputEl.value} kilos`
+
+})
